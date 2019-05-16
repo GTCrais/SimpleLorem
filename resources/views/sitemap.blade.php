54fa9ec48dde5c4ae3db4ba7e4ad2500724bded9
GTCrais <tomislav@gtcrais.net>
2019-05-16 12:11:07 +0200

    Update sitemap.

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -6,4 +6,10 @@
 		<priority>1.0</priority>
 	</url>
 
+	<url>
+		<loc>http://www.simplelorem.com/</loc>
+		<lastmod>2019-05-16T10:00:00+01:00</lastmod>
+		<priority>0.7</priority>
+	</url>
+
 </urlset>
